6bef40de24ab15fb74ea5c1df8a7bea645c8edf9
Shayne Kandagor <Shayne.Kandagor@metropolia.fi>
2022-11-01 15:01:06 +0200

    added basic dummy routing

--- week-two/app.js
+++ week-two/app.js
@@ -4,11 +4,26 @@ const app = express();
 const port = 3000;
 
 app.get('/cat', (req, res) => {
+   console.log(req);
   res.send('From this endpoint you can get cats.')
 });
 
-app.get('/user', (req, res) => {
-  res.send('From this endpoint you can get users.')
+app.get('/cat/:catId', (req, res) => {
+  // console.log(req.params);
+  res.send('From this endpoint you can get cat with Id' + req.params.catId);
 });
+
+app.post('/cat', (req, res) => {
+  res.send('From this endpoint you can add more cats.');
+});
+
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can put cats.')
+});
+
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete cats.')
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
