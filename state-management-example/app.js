445328f19c15c916d3cdcc21b918618d8bfa03d1
Shayne Kandagor <Shayne.Kandagor@metropolia.fi>
2022-11-22 14:27:11 +0200

    cookie task done and session task done

--- state-management-example/app.js
+++ state-management-example/app.js
@@ -1,15 +1,49 @@
 'use strict';
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3000;
 
+const user = {
+  username:'foo',
+  password:'bar'
+};
+
+//Global variable for the server
+let loggedIn = false;
+
+
 app.set('views', './views');
 app.set('view engine', 'pug');
-app.use(cookiePaser());
+app.use(cookieParser());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({extended: true})); // for form data
 
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if(loggedIn) {
+    res.render('secret'); 
+  }else{
+    res.redirect('/form');
+  }
+});
+
+app.post('/login', (req, res) => {
+  // check for username / password match
+  console.log(req.body);
+  if(req.body.username == user.username && req.body.password == user.password){
+    loggedIn = true;
+    res.redirect('/secret');
+  } 
+});
+
 
 app.get('/getCookie', (req, res) => {
   console.log(req.cookies)
