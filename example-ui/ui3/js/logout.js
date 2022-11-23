52c2cc35734bca32db00cba46d697a2d80c2ecf5
Shayne Kandagor <Shayne.Kandagor@metropolia.fi>
2022-11-23 11:34:30 +0200

    wip: add user/ role based authorization

--- example-ui/ui3/js/logout.js
+++ example-ui/ui3/js/logout.js
@@ -3,9 +3,10 @@ const url = 'http://localhost:3000'; // change url when uploading to server
 
 (async () => {
   try {
-    const response = await fetch(url + '/auth/logout');
-    const json = await response.json();
-    console.log(json);
+    //const response = await fetch(url + '/auth/logout');
+    //const json = await response.json();
+    //console.log(json);
+    
     // remove token
     sessionStorage.removeItem('token');
     sessionStorage.removeItem('user');
